cd1f1b168041aceffa20850daa1fd4bcab907f76
Hannah Stapleton <114391681+hannahStapleton@users.noreply.github.com>
2023-11-03 18:30:35 +0000

    Add files via upload

---
+++ WEB-INF/src/com/c20321466/distributedsystems/UserDAO.java
@@ -0,0 +1,67 @@
+package com.c20321466.distributedsystems;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.c20321466.distributedsystems.model.User;
+
+
+	public class UserDAO {
+		
+		protected static EntityManagerFactory emf = 
+		Persistence.createEntityManagerFactory("moneylenders");
+
+		public UserDAO() {
+		}
+		 	
+
+		public void persist(User user) {
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			em.persist(user);
+			em.getTransaction().commit();
+			em.close();
+		}
+		
+		public void removeUser(User user) {
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			em.remove(em.merge(user));
+			em.getTransaction().commit();
+			em.close();
+		}
+		
+		public User merge(User user) {
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			User updatedUser = em.merge(user);
+			em.getTransaction().commit();
+			em.close();
+			return updatedUser;
+		}
+		
+		
+		public List<User> getAllUsers() {
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			List<User> users = new ArrayList<User>();
+			users = em.createQuery("from User").getResultList();
+			em.getTransaction().commit();
+			em.close();
+			return users;
+		}
+
+		public User getUserById(int userId) {
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			User e = em.createQuery("SELECT p FROM User p WHERE p.userId = :userId", User.class)
+	                .setParameter("userId", userId)
+	                .getSingleResult();
+			em.getTransaction().commit();
+			em.close();
+			return e;
+		}
+
+	}
